5f9dd553ece78fd3525866a5b0681282776201e5
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-11-17 11:18:41 -0500

    ELIS-9293: Update versions release & build date.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015051102;
-$plugin->release = '2.9.2.1 (Build: 20151021)';
+$plugin->release = '2.9.3.0 (Build: 20151117)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'local_eliscore' => 2015051102
